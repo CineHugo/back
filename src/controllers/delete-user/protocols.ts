98b348f157d0a7ad6fcf7799a179a41506512c92
Dilceu Lopes <dilceulopesjr181@gmail.com>
2025-05-09 23:38:39 -0300

    [ADD]DeleteUsercontroller e rota

--- src/controllers/delete-user/protocols.ts
+++ src/controllers/delete-user/protocols.ts
@@ -1,5 +1,11 @@
+/* eslint-disable no-redeclare */
 /* eslint-disable no-unused-vars */
 import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
+
+export interface IDeleteUserController {
+    handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>>;
+}
 
 export interface IDeleteUserRepository {
     deleteUser(id: string): Promise<User>;
