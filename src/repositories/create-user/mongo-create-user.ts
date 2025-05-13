1574e4138a06f54df461f7600562a7a4e58e95e1
Dilceu Lopes <dilceulopesjr181@gmail.com>
2025-05-13 10:53:42 -0300

    [ADD]Email in use validation

--- src/repositories/create-user/mongo-create-user.ts
+++ src/repositories/create-user/mongo-create-user.ts
@@ -11,6 +11,12 @@ export class MongoCreateUserRepository implements ICreateUserRepository {
   async createUser(params: CreateUserParams): Promise<User> {
     const now = new Date();
 
+    const emailExists = await MongoClient.db.collection("users").findOne({ email: params.email});
+
+    if(emailExists){
+      throw new Error("Email already in use");
+    }
+
     const password = await bcrypt.hash(params.password, 10);
 
     // Completa os dados com os campos obrigatórios
@@ -23,6 +29,8 @@ export class MongoCreateUserRepository implements ICreateUserRepository {
       updatedAt: now,
       deletedAt: null,
     };
+
+    console.log(userData);
 
     const { insertedId } = await MongoClient.db
       .collection("users")
