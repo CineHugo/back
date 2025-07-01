3854f583752d1b845d7ecd51c38a7ac209355144
lopesDilceu <dilceulopesjr181@gmail.com>
2025-06-30 22:49:01 -0300

    fix: update movie with images

--- src/controllers/movie/update-movie/update-movie.ts
+++ src/controllers/movie/update-movie/update-movie.ts
@@ -1,24 +1,43 @@
 /* eslint-disable no-unused-vars */
 import { Movie } from "../../../models/movie";
+import { Base64Image, ImageStorageService } from "../../../services/image-storage-service";
 import { badRequest, ok, serverError } from "../../helpers";
 import { HttpRequest, HttpResponse, IController } from "../../protocols";
 import { IUpdateMovieRepository, UpdateMovieParams } from "./protocols";
+
+// Estende temporariamente os campos que podem vir no body
+interface UpdateMovieRequest
+  extends Omit<UpdateMovieParams, "mainImageUrl" | "bannerImageUrl"> {
+  mainImageUrl?: Base64Image;
+  bannerImageUrl?: Base64Image;
+}
 
 export class UpdateMovieController implements IController {
   constructor(private readonly updateMovieRepository: IUpdateMovieRepository) {}
 
   async handle(
-    httpRequest: HttpRequest<UpdateMovieParams>
+    httpRequest: HttpRequest<UpdateMovieRequest>
   ): Promise<HttpResponse<Movie | string>> {
     try {
       const id = httpRequest?.params?.id;
-      const body = httpRequest?.body;
-      if (!body) {
-        return badRequest("Body missing fields");
-      }
+      const { title, synopsis, releaseDate, mainImageUrl, bannerImageUrl, ...rest } = httpRequest?.body || {};
 
-      if (!id) {
-        return badRequest("Missing movie id");
+      if (!id) return badRequest("Missing movie id");
+
+      const bodyToUpdate: UpdateMovieParams = {
+        ...rest,
+      };
+
+      if (title) bodyToUpdate.title = title;
+      if (synopsis) bodyToUpdate.synopsis = synopsis;
+      if (releaseDate) bodyToUpdate.releaseDate = releaseDate;
+
+      // Processar imagens base64, se enviadas
+      if (mainImageUrl) {
+        bodyToUpdate.mainImageUrl = await ImageStorageService.save(mainImageUrl);
+      }
+      if (bannerImageUrl) {
+        bodyToUpdate.bannerImageUrl = await ImageStorageService.save(bannerImageUrl);
       }
 
       const allowedFieldsToUpdate: (keyof UpdateMovieParams)[] = [
@@ -29,7 +48,7 @@ export class UpdateMovieController implements IController {
         "bannerImageUrl",
       ];
 
-      const someFieldsAreNotAllowedToUpdate = Object.keys(body).some(
+      const someFieldsAreNotAllowedToUpdate = Object.keys(bodyToUpdate).some(
         (key) => !allowedFieldsToUpdate.includes(key as keyof UpdateMovieParams)
       );
 
@@ -37,7 +56,7 @@ export class UpdateMovieController implements IController {
         return badRequest("Some fields are not allowed to be updated");
       }
 
-      const movie = await this.updateMovieRepository.updateMovie(id, body);
+      const movie = await this.updateMovieRepository.updateMovie(id, bodyToUpdate);
 
       if (!movie) {
         return badRequest("Movie not found or no fields to update");
@@ -45,6 +64,7 @@ export class UpdateMovieController implements IController {
 
       return ok<Movie>(movie);
     } catch (error) {
+      console.error("Erro no UpdateMovieController:", error);
       return serverError();
     }
   }
